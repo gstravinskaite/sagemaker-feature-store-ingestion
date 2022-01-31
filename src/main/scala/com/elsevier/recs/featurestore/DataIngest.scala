c12f15b4c1a02dd4bfe082209ba05f3bf71b58b2
gstravinskaite <g.stravinskaite@elsevier.com>
2022-01-31 13:07:57 +0100

    working Spark3 solution

--- src/main/scala/com/elsevier/recs/featurestore/DataIngest.scala
+++ src/main/scala/com/elsevier/recs/featurestore/DataIngest.scala
@@ -2,31 +2,62 @@ package com.elsevier.recs.featurestore
 import com.elsevier.recs.featurestore.client.SparkClient
 import org.apache.spark.sql
 import org.apache.spark.sql.functions.{current_timestamp, date_format}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import software.amazon.sagemaker.featurestore.sparksdk.FeatureStoreManager
 
 
 case object DataIngest extends SparkClient {
 
   def main(args: Array[String]): Unit = {
-    val filePath = System.getProperty("user.dir") + "/src/main/resources/data/part-00000-5f001ccd-45f5-42fc-abf3-615f6fcbe4f6-c000.snappy.parquet"
-    val jsonPath = System.getProperty("user.dir") + "/src/main/resources/ABREP_2019_204__bigrams_logistic-regression.json"
-    val data = readAndProcessLambdaJson(jsonPath)
-    println(ingestDataToFeatureStore(data))
+//    val filePath = System.getProperty("user.dir") + "/src/main/resources/data/part-00000-5f001ccd-45f5-42fc-abf3-615f6fcbe4f6-c000.snappy.parquet"
+    ingestStaticData()
   }
 
 
   def readAndProcessLambdaJson(filepath: String): sql.DataFrame = {
     val fullData = sparkSession.read.json(filepath)
 
+    println(s"Size:${fullData.count()}")
+
     val neededData = fullData.selectExpr(
-      "numPublications", "numPubsThisYear", "numPubsLast5Year", "name", "surname", "scopusId", "email",
-    ).withColumnRenamed("name", "givenName")
-      .withColumnRenamed("scopusId","id")
+      "numPublications", "numPubsThisYear", "numPubsLast5Year", "givenName", "surname", "id", "email",
+    )
+//    val neededData = fullData.selectExpr(
+//      "numPublications", "numPubsThisYear", "numPubsLast5Year", "name", "surname", "scopusId", "email",
+//    ).withColumnRenamed("name", "givenName")
+//      .withColumnRenamed("scopusId","id")
 
     val timestampedData = neededData.withColumn(
       "EventTime", date_format(current_timestamp(), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))
 
     timestampedData
+  }
+
+  def ingestStaticData(): Unit = {
+
+    val data = List(
+      sql.Row(100 , 50, 100, "Gabriele", "Stravinskaite", "888888", "gabby@vilnius.com"),
+      sql.Row(110 , 40, 110, "Jon", "Snow", "777777", "jon@wall.com"),
+      sql.Row(90, 30, 200, "Harry", "Potter", "666666", "harry@scotland.com")
+    )
+
+    val schema = StructType(
+      List(StructField("numPublications", IntegerType),
+        StructField("numPubsThisYear", IntegerType),
+        StructField("numPubsLast5Year", IntegerType),
+        StructField("givenName", StringType),
+        StructField("surname", StringType),
+        StructField("id", StringType),
+        StructField("email", StringType),
+      ))
+
+    val df = sparkSession.createDataFrame(sparkSession.sparkContext.parallelize(data), schema)
+
+    val timestampedData = df.withColumn(
+      "EventTime", date_format(current_timestamp(), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))
+
+    ingestDataToFeatureStore(timestampedData)
+
   }
 
   def readAndProcessData(filepath: String): sql.DataFrame = {
--- src/main/scala/com/elsevier/recs/featurestore/client/SparkClient.scala
+++ src/main/scala/com/elsevier/recs/featurestore/client/SparkClient.scala
@@ -1,21 +1,30 @@
 package com.elsevier.recs.featurestore.client
 
 import org.apache.log4j.{Level, LogManager}
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
 
-trait SparkClient  {
+trait SparkClient {
 
   implicit var sparkSession: SparkSession = _
 
+  val registerKryoClasses = Array[Class[_]]()
+
   sparkSession = SparkSession.builder()
-    .master("local[2]")
-    .appName(getClass.getSimpleName)
-    .config("spark.driver.host", "127.0.0.1")
-    .config("spark.ui.enabled", "false")
+    .appName("FeatureStoreIngestion")
+//    .master("local[2]")
+//    .config("spark.driver.host", "127.0.0.1")
+//    .config("spark.ui.enabled", "false")
+    .config(
+      new SparkConf()
+        .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+        .registerKryoClasses(registerKryoClasses)
+    )
     .getOrCreate()
 
   LogManager.getRootLogger.setLevel(Level.ERROR)
 
   sparkSession.sql("set spark.sql.shuffle.partitions=4")
 }
+
